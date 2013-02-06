d53528b46c06e36189ac8052a04196948fbdda7c
Joshua Langowitz <jlangowitz@Garrus.(none)>
2013-02-05 22:44:58 -0500

    fixed the last thing, still need new twit hiding and css formatting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var models = require('../models')
 	,Twit = models.Twit;
 
 exports.index = function(req, res){
-	findTwits().exec(function(err,data){
+	Twit.find().sort({'_id' : 'descending'}).exec(function(err,data){
 		if (err)
 			return console.log ('error', err);
 		res.render('index', {title: 'Crappy Twitter', twits: data});
@@ -11,13 +11,9 @@ exports.index = function(req, res){
 };
 
 exports.update = function(req, res){
-	findTwits().exec(function(err,data){
+	Twit.find().sort({'_id' : 'descending'}).exec(function(err,data){
 		if (err)
 			return console.log ('error', err);
 		res.render('_twits', {twits: data});
 	});
 };
-
-function findTwits(){
-	return Twit.find().sort({'_id' : 'descending'});
-}
